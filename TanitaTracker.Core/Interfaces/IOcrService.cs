fb828ca89c4f6b90c3edbfe2b5239501056c2c37
Ron Spierings <rjl.spierings@avans.nl>
2026-04-20 22:13:42 +0200

    Create IOcrServices for Azure Document Intelligence usages

---
+++ TanitaTracker.Core/Interfaces/IOcrService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TanitaTracker.Core.Entities;
+
+namespace TanitaTracker.Core.Interfaces
+{
+    public interface IOcrService
+    {
+        /// <summary>
+        /// Analyzes a stream (image/pdf) using the custom Tanita OCR model and maps it to the domain entity.
+        /// </summary>
+        Task<BodyCompositionScan> AnalyzeScanAsync(Stream documentStream, CancellationToken cancellationToken = default);
+    }
+}
