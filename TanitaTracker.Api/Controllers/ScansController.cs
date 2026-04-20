de87f7862058133ea2708aa97def9db636df45b9
Ron Spierings <rjl.spierings@avans.nl>
2026-04-20 23:04:11 +0200

    Created a API controller called ScansController and wrote 3 unimplemented endpoints. Also setup basic builder configuration and removed some clutter (WeatherForecaster stuff).

---
+++ TanitaTracker.Api/Controllers/ScansController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TanitaTracker.Core.Entities;
+using TanitaTracker.Core.Interfaces;
+
+namespace TanitaTracker.Api.Controllers
+{
+    /// <summary>
+    /// Implementation of the MVC based Scan Controller endpoint (/api/ScanController). 
+    /// </summary>
+    /// [Authorize]
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")] // Endpoint definition
+    public class ScansController : ControllerBase
+    {
+        private readonly IScanRepository _repository; // Via Dependency Injection a ScanRepository object is created
+        private readonly IOcrService _ocrService; // // Via Dependency Injection a OcrService object is created
+
+        public ScansController(IScanRepository repository, IOcrService ocrService)
+        {
+            _repository = repository;
+            _ocrService = ocrService;
+        }
+
+        // Helper to securely get the logged-in user's ID. See Claims for the definition
+        private string GetUserId() => User.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty;
+
+        [HttpPost("analyze")]
+        public async Task<ActionResult<BodyCompositionScan>> AnalyzeDocument(IFormFile file, CancellationToken ct)
+        {
+            // TODO: Write a implementation
+            throw new NotImplementedException("Not yet impemented");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BodyCompositionScan>>> GetMyScans(CancellationToken ct)
+        {
+            // TODO: Write implementation
+            throw new NotImplementedException("Not yet implemented");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveScan([FromBody] BodyCompositionScan scan, CancellationToken ct)
+        {
+            // TODO: Write implementation
+            throw new NotImplementedException("Not yet implemented");
+        }
+
+    }
+}
--- TanitaTracker.Api/Program.cs
+++ TanitaTracker.Api/Program.cs
@@ -1,8 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>();
+
+// TODO: Dependency Injections when done
+// TODO: Add ApplicationDBContext when done
+
+// CORS configuration policy so Blazor WebAssembly can communicate with this API
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorApp", policy =>
+    {
+        policy.WithOrigins("https://localhost:7000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -13,29 +32,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowBlazorApp"); // Link the policy to the App
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Add Authenticaion and Authorization
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Map the Identity endpoints (Login, Register, etc.)
+app.MapIdentityApi<IdentityUser>();
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
