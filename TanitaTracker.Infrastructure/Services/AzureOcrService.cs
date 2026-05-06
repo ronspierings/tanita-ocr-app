877fbfdf633d2a856ec2769d9b4a7fa38ca593d8
Ron Spierings <rjl.spierings@avans.nl>
2026-05-07 01:25:37 +0200

    Forgot the // TODO

--- TanitaTracker.Infrastructure/Services/AzureOcrService.cs
+++ TanitaTracker.Infrastructure/Services/AzureOcrService.cs
@@ -11,6 +11,7 @@ namespace TanitaTracker.Infrastructure.Services
     {
         public Task<BodyCompositionScan> AnalyzeScanAsync(Stream documentStream, CancellationToken cancellationToken = default)
         {
+            // TODO: Write the implementation
             throw new NotImplementedException("Work in progress (as in: Azure Document Intelligence Setup... ");
         }
     }
